f69c1a8f3c61d65619327df018138c47a839424f
Shweta Sharma <sshweta2407@gmail.com>
2020-08-03 13:00:29 +0530

    Authentication Code for Twitter

--- Twitter Analysis/twitter_authenticate.py
+++ Twitter Analysis/twitter_authenticate.py
@@ -4,21 +4,22 @@ from requests_oauthlib import OAuth1Session, OAuth1
 from urllib.request import urlopen
 from urllib.parse import parse_qs, quote_plus
 import json
-from database_operations import check_twitter_details, save_twitter_details, update_twitter_details
 
+#various urls for twitter authentication
 request_url = "https://api.twitter.com/oauth/request_token"
 auth_url = "https://api.twitter.com/oauth/authorize"
 access_url = "https://api.twitter.com/oauth/access_token"
 update_url = "http://api.twitter.com/1/statuses/update.json"
 
-key = Twitter_key.consumer_key
-secret = Twitter_key.consumer_secret
+key = #add consumer key here from developer account
+secret = #add consumer secret key here from the twitter developer account
 
-bearer_token =Twitter_key.bearer_token
+bearer_token = #insert bearer token from the twitter developer account
 search_headers = {
     'Authorization': 'Bearer {}'.format(bearer_token)    
 }
 
+#call this function on the route , it will direct you to the twitter authentication page
 def add_twitter():
 	twitter = OAuth1Session(client_key= key, client_secret=secret)
 	r = twitter.fetch_request_token(request_url)
@@ -27,7 +28,7 @@ def add_twitter():
 	auth = twitter.authorization_url(auth_url)
 	return oauth_token, oauth_secret, auth
 
-
+#this function will returns the access tokens of the authentication of the user 
 def twitter_callback(token, oauth_secret, verifier, spear_id, email_id):
 	twitter = OAuth1Session(key, client_secret=secret,                       resource_owner_key=token,resource_owner_secret=oauth_secret, verifier=verifier)
 	response = twitter.fetch_access_token(access_url)
@@ -37,15 +38,23 @@ def twitter_callback(token, oauth_secret, verifier, spear_id, email_id):
 	info = twitter_info(access_token, token_secret)
 
 	print(info)
-
-	if a == "no":
-		save_twitter_details(access_token, token_secret, response["screen_name"],response["user_id"], spear_id, info, email_id)
-
-	else:
-		update_twitter_details(response["user_id"], response["screen_name"], info, email_id, access_token, token_secret)
 	return "done"
 	
+#this function will return the basic info like screenname, name and profile data of the user
+def twitter_info(access_token, token_secret):
 
+	twitter = OAuth1( client_key=key,
+			client_secret=secret,
+			resource_owner_key= access_token,
+			resource_owner_secret= token_secret)
+
+	url ='https://api.twitter.com/1.1/account/verify_credentials.json'
+	resp = requests.get(url, auth=twitter)
+	info = json.loads(resp.text)
+
+	return info
+
+#This function will post the tweet on the user account using his access tokens and your developer keys.
 def twitter_upload(text, access_token, access_token_secret):
 	twitter = OAuth1(client_key = key, 
                         client_secret = secret,
@@ -62,16 +71,4 @@ def twitter_upload(text, access_token, access_token_secret):
 		return {'success':True}
 
 
-def twitter_info(access_token, token_secret):
-
-	twitter = OAuth1( client_key=key,
-			client_secret=secret,
-			resource_owner_key= access_token,
-			resource_owner_secret= token_secret)
-
-	url ='https://api.twitter.com/1.1/account/verify_credentials.json'
-	resp = requests.get(url, auth=twitter)
-	info = json.loads(resp.text)
-
-	return info
 
